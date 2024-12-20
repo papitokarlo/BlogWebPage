951e598bee07f53c2a4366e5da59b13b62939146
zura <zura_begadze>
2024-12-20 15:50:52 +0400

    refactor: forget to add categories urls

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import BlogViewSet, CommentViewSet, TagViewSet, MenuViewSet
-
+from .views import BlogViewSet, CommentViewSet, TagViewSet, MenuViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'blogs', BlogViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'tags', TagViewSet)
 router.register(r'menus', MenuViewSet)
+router.register(r'categories', CategoryViewSet)
 
 
 urlpatterns = router.urls
