c2595004c40981b94e3f77db9fdfb214b80a778c
zura <zura_begadze>
2024-12-20 12:14:19 +0400

    fixed: remove my gmail creds

--- server/settings.py
+++ server/settings.py
@@ -168,8 +168,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "begadze.zura@gmail.com"
-EMAIL_HOST_PASSWORD = "paroli321"
+EMAIL_HOST_USER = "gmail here"
+EMAIL_HOST_PASSWORD = "password here"
 
 TINYMCE_DEFAULT_CONFIG = {
     "height": 500,
