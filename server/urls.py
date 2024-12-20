ec8ea2fa02a53ec49ffd56f0b2e6aff7a5505e82
zura <zura_begadze>
2024-12-20 11:46:33 +0400

    fixed: url for documentation

--- server/urls.py
+++ server/urls.py
@@ -18,19 +18,21 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.permissions import IsAdminUser
 
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API Title",
+        title="News Room",
         default_version='v1',
-        description="Your API description",
+        description="News room api",
     ),
-    public=True,
+    public=False,
+    permission_classes=[IsAdminUser],
 )
 
 urlpatterns = [
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('apps.urls')),
 ]
